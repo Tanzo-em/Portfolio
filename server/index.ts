85c46bbb9f0dd704e289d606b9585401dde99d00
Rakesh <rakesh.rkt121@gmail.com>
2025-10-11 14:23:09 +0530

    init

--- server/index.ts
+++ server/index.ts
@@ -10,34 +10,8 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Basic API logging (dev only)
-app.use((req: Request, res: Response, next: NextFunction) => {
-  const start = Date.now();
-  const path = req.path;
-  let capturedJsonResponse: Record<string, any> | undefined = undefined;
 
-  const originalResJson = res.json;
-  // @ts-ignore
-  res.json = function (bodyJson, ...args) {
-    capturedJsonResponse = bodyJson;
-    // @ts-ignore
-    return originalResJson.apply(res, [bodyJson, ...args]);
-  };
 
-  res.on('finish', () => {
-    const duration = Date.now() - start;
-    if (path.startsWith('/api')) {
-      let logLine = `${req.method} ${path} ${res.statusCode} in ${duration}ms`;
-      if (capturedJsonResponse) {
-        logLine += ` ${JSON.stringify(capturedJsonResponse)}`;
-      }
-      if (logLine.length > 80) logLine = logLine.slice(0, 79);
-      log(logLine, 'express');
-    }
-  });
-
-  next();
-});
 
 async function main() {
 
